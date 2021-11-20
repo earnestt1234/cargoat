2e7f86e475c35d5bb03d99038cdcb3c7712a2085
Tom Earnest <earnestt1234@gmail.com>
2021-11-20 15:57:43 -0600

    working on weighted pick doors

--- cargoat/steps.py
+++ cargoat/steps.py
@@ -4,12 +4,15 @@
 Classes for individual game rules applied to `MontyHallSim` objects.
 """
 
+from collections.abc import Iterable
+from numbers import Number
 import pprint
 
 import numpy as np
 
 from cargoat.errors import (BadPick, BadReveal)
 
+# ---- Parent class
 class MontyHallRule(object):
     def __repr__(self):
         name = self.__class__.__name__
@@ -21,6 +24,7 @@ class MontyHallRule(object):
         n = len(idx)
         raise errortype(f"{msg} Found for {n} trial(s):\n{idx}")
 
+# ---- Initialization
 class InitDoorsRandom(MontyHallRule):
     def __init__(self, cars=1, goats=2):
         self.cars = cars
@@ -35,19 +39,7 @@ class InitDoorsRandom(MontyHallRule):
         p = np.random.rand(shape[0], shape[1]).argsort(1)
         sim.cars[p < self.cars] = 1
 
-# class PickDoorN(MontyHallRule):
-#     def __init__(self, door):
-#         self.door = door
-
-#     def __call__(self, sim):
-#         prior = sim.picked.copy()
-#         sim.picked[sim.idx, self.door] = 1
-
-#         badpicks = np.logical_and(sim.picked - prior, sim.revealed)
-#         if np.any(badpicks):
-#             msg = "A revealed door was picked."
-#             self.bad_trials_raise(np.any(badpicks, axis=1), msg, BadPick)
-
+# ---- Picking Doors
 class PickDoor(MontyHallRule):
     def __call__(self, sim):
         pickable = sim.pickable_doors()
@@ -62,6 +54,56 @@ class PickDoor(MontyHallRule):
         newpicks[sim.idx, to_pick] = 1
         sim.picked = newpicks.astype(int)
 
+class PickDoorWeighted(MontyHallRule):
+    def __init__(self, weights):
+        if self.weights_type_okay(weights):
+            self.weights = weights
+        else:
+            raise TypeError("weights must be None, a number, or an "
+                            f"iterable of numbers, not {type(weights)}")
+
+    def __call__(self, sim):
+        w = self.weights
+        if isinstance(w, Iterable) and len(w) == sim.shape[1]:
+            pass
+        else:
+            raise NotImplementedError("Only implemented weights of same "
+                                      "length as number of doors.")
+
+    def weights_type_okay(self, weights):
+        ok1 = weights is None
+        ok2 = isinstance(weights, Number)
+
+        isiterable = isinstance(weights, Iterable)
+        if isiterable:
+            nonnull = len(weights) >= 1
+            numbers = all(isinstance(i, Number) for i in weights)
+            ok3 = nonnull and numbers
+
+        return any([ok1, ok2, ok3])
+
+class Stay(MontyHallRule):
+    def __call__(self, sim):
+        pass
+
+class Switch(MontyHallRule):
+    def __call__(self, sim):
+        PickDoor()(sim)
+
+# class PickDoorN(MontyHallRule):
+#     def __init__(self, door):
+#         self.door = door
+
+#     def __call__(self, sim):
+#         prior = sim.picked.copy()
+#         sim.picked[sim.idx, self.door] = 1
+
+#         badpicks = np.logical_and(sim.picked - prior, sim.revealed)
+#         if np.any(badpicks):
+#             msg = "A revealed door was picked."
+#             self.bad_trials_raise(np.any(badpicks, axis=1), msg, BadPick)
+
+# ---- Revealing Doors
 class RevealGoat(MontyHallRule):
     def __call__(self, sim):
         revealable = sim.revealable_doors()
@@ -74,14 +116,7 @@ class RevealGoat(MontyHallRule):
         to_reveal = weights.argmax(1)
         sim.revealed[sim.idx, to_reveal] = 1
 
-class Stay(MontyHallRule):
-    def __call__(self, sim):
-        pass
-
-class Switch(MontyHallRule):
-    def __call__(self, sim):
-        PickDoor()(sim)
-
+# ---- Other
 class Finish(MontyHallRule):
     def __call__(self, sim):
         pprint.pprint(sim.get_results(), sort_dicts=False)
