617901709308e24f57c6fc526b821a7074ea4439
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2021-11-25 16:07:43 -0600

    thanksgiving work!
    add spoiled attribute for keeping track of ruined games,
    add set_picks method for handling setting picks and managing spoiling, add e.g. doors into error messsages, add PickDoors & PickSpecificDoors

--- cargoat/sim.py
+++ cargoat/sim.py
@@ -16,7 +16,9 @@ class MontyHallSim:
         self.cars = np.empty(0)
         self.picked = np.empty(0)
         self.revealed = np.empty(0)
+        self.spoiled = np.empty(0)
 
+    # ---- Properties
     @property
     def idx(self):
         return np.arange(self.n)
@@ -25,9 +27,10 @@ class MontyHallSim:
     def shape(self):
         return self.cars.shape
 
+    # ---- Status of the sim
     def pickable_doors(self, exclude_current=True):
         if exclude_current:
-            return 1 - (self.picked + self.revealed)
+            return (~np.logical_or(self.picked, self.revealed)).astype(int)
         else:
             return 1 - self.revealed
 
@@ -35,24 +38,35 @@ class MontyHallSim:
         rd = ~np.logical_or.reduce([self.cars, self.picked, self.revealed])
         return rd.astype(int)
 
+    # ---- Handling door picking
+    def set_picks(self, picks, add=False, allow_spoiled=True):
+        valid = self.validate_picks(picks)
+        badrows = np.any(~valid, axis=1)
+        if not allow_spoiled and np.any(~valid):
+            trial, door = self.get_index_success(~valid)
+            msg = ("Revealed doors were picked, e.g. "
+                   f"trial {trial} door {door}.")
+            self.bad_trials_raise(badrows, msg, BadPick)
+
+        self.spoiled[badrows] = 1
+        if add:
+            picks = np.logical_or(picks, self.picked).astype(int)
+        self.picked = picks
+
     def validate_picks(self, picks):
         return ~ np.logical_and(self.revealed, picks)
 
+    # ---- Generic errors
     def bad_trials_raise(self, badrows, msg, errortype):
         idx = np.arange(len(badrows))[badrows]
         n = len(idx)
         raise errortype(f"{msg} Found for {n} trial(s):\n{idx}")
 
-    def set_picks(self, picks, raise_badpicks=True):
-        if raise_badpicks:
-            valid = self.validate_picks(picks)
-            if np.any(~valid):
-                badrows = np.any(~valid, axis=1)
-                msg = "Revealed doors were picked."
-                self.bad_trials_raise(badrows, msg, BadPick)
+    # ---- Other helpers
+    def get_index_success(self, boolarray, i=0):
+        return np.asarray(np.where(boolarray)).T[i]
 
-        self.picked = picks
-
+    # ---- Results
     def get_results(self):
         wins = np.sum(np.any(self.picked * self.cars, axis=1))
         losses = self.n - wins
@@ -67,3 +81,5 @@ class MontyHallSim:
             }
 
         return results
+
+
--- cargoat/steps.py
+++ cargoat/steps.py
@@ -5,7 +5,6 @@ Classes for individual game rules applied to `MontyHallSim` objects.
 """
 
 from collections.abc import Iterable
-from numbers import Number
 import pprint
 
 import numpy as np
@@ -30,32 +29,63 @@ class InitDoorsRandom(MontyHallRule):
         sim.cars = np.zeros(shape, dtype=int)
         sim.picked = np.zeros(shape, dtype=int)
         sim.revealed = np.zeros(shape, dtype=int)
+        sim.spoiled = np.zeros(sim.n, dtype=int)
 
         p = np.random.rand(shape[0], shape[1]).argsort(1)
         sim.cars[p < self.cars] = 1
 
 # ---- Picking Doors
 class PickDoor(MontyHallRule):
+    def __init__(self, exclude_current=True, add=False):
+        self.exclude_current = exclude_current
+        self.add = add
+
     def __call__(self, sim):
 
         # enforce a door is picked
-        pickable = sim.pickable_doors()
+        pickable = sim.pickable_doors(self.exclude_current)
         badrows = ~np.any(pickable, axis=1)
         if np.any(badrows):
-            msg = "No pickable doors."
+            msg = "Some trials have no pickable doors."
             sim.bad_trials_raise(badrows, msg, BadPick)
 
-        newpicks = np.zeros(sim.shape)
+        newpicks = np.zeros(sim.shape, dtype=int)
         weights = np.random.rand(*sim.shape) * pickable
         to_pick = weights.argmax(1)
         newpicks[sim.idx, to_pick] = 1
-        sim.set_picks(newpicks)
+        sim.set_picks(newpicks, add=self.add)
+
+class PickDoors(MontyHallRule):
+    def __init__(self, n, exclude_current=True, add=False):
+        self.n = n
+        self.exclude_current = exclude_current
+        self.add = add
+
+    def __call__(self, sim):
+        # enforce n doors are picked
+        pickable = sim.pickable_doors(self.exclude_current)
+        n_pickable = pickable.sum(axis=1)
+        badrows = (n_pickable < self.n)
+        if np.any(badrows):
+            i = badrows.argmin()
+            msg = (f"Some trials have less than {self.n} pickable doors, "
+                   f"e.g. trial {i} with {n_pickable[i]}.")
+            sim.bad_trials_raise(badrows, msg, BadPick)
+
+        newpicks = np.zeros(sim.shape, dtype=int)
+        weights = np.random.rand(*sim.shape) * pickable
+        indices = weights.argsort(1)
+        newpicks[indices < self.n] = 1
+        sim.set_picks(newpicks, add=self.add)
 
 class PickDoorWeighted(MontyHallRule):
-    def __init__(self, weights, unweight_revealed=True, unweight_picked=True):
+    def __init__(self, weights, unweight_revealed=True, unweight_picked=True,
+                 allow_spoiled=False, add=False):
         self.weights = weights
         self.unweight_revealed = unweight_revealed
         self.unweight_picked = unweight_picked
+        self.allow_spoiled = allow_spoiled
+        self.add = add
 
     def __call__(self, sim):
         w = self.weights
@@ -94,7 +124,20 @@ class PickDoorWeighted(MontyHallRule):
 
         picks = np.zeros(sim.shape, dtype=int)
         picks[sim.idx, to_pick] = 1
-        sim.set_picks(picks, raise_badpicks=True)
+        sim.set_picks(picks, allow_spoiled=self.allow_spoiled, add=self.add)
+
+class PickSpecificDoors(MontyHallRule):
+    def __init__(self, doors, add=False, allow_spoiled=False):
+        if not isinstance(doors, Iterable):
+            doors = [doors]
+        self.doors = list(doors)
+        self.add = add
+        self.allow_spoiled = allow_spoiled
+
+    def __call__(self, sim):
+        newpicks = np.zeros(sim.shape, dtype=int)
+        newpicks[:, self.doors] = 1
+        sim.set_picks(newpicks, add=self.add, allow_spoiled=self.allow_spoiled)
 
 class Stay(MontyHallRule):
     def __call__(self, sim):
@@ -103,19 +146,6 @@ class Stay(MontyHallRule):
 class Switch(MontyHallRule):
     def __call__(self, sim):
         PickDoor()(sim)
-
-# class PickDoorN(MontyHallRule):
-#     def __init__(self, door):
-#         self.door = door
-
-#     def __call__(self, sim):
-#         prior = sim.picked.copy()
-#         sim.picked[sim.idx, self.door] = 1
-
-#         badpicks = np.logical_and(sim.picked - prior, sim.revealed)
-#         if np.any(badpicks):
-#             msg = "A revealed door was picked."
-#             sim.bad_trials_raise(np.any(badpicks, axis=1), msg, BadPick)
 
 # ---- Revealing Doors
 class RevealGoat(MontyHallRule):
