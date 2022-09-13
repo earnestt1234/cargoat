38757429c10f5cfae47c4dbc0ac60dc6413b0be8
Tom Earnest <earnestt1234@gmail.com>
2022-09-12 22:47:47 -0500

    add close specific door

--- cargoat/steps.py
+++ cargoat/steps.py
@@ -241,6 +241,25 @@ class CloseDoors(MontyHallRule):
         newreveals = (closable - to_close).astype(int)
         sim.set_revealed(newreveals, add=False)
 
+class CloseSpecificDoors(MontyHallRule):
+    def __init__(self, doors):
+        if not isinstance(doors, Iterable):
+            doors = [doors]
+        self.doors = list(doors)
+
+    def __call__(self, sim):
+        check_closable = sim.revealed[:, self.doors]
+        badrows = np.any(check_closable != 1, axis=1)
+        if np.any(badrows):
+            msg = f'At least some doors at positions {self.doors} are not open.'
+            bad_trials_raise(badrows, msg, BadClose)
+
+        to_close = np.zeros(sim.shape, dtype=int)
+        to_close[:, self.doors] = 1
+        newreveals = (sim.revealed - to_close).astype(int)
+        sim.set_revealed(newreveals, add=False)
+
+
 # ---- Other
 class Finish(MontyHallRule):
     def __call__(self, sim):
