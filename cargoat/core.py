ef614b73fec8d75d1c82fe0929ddb67001eab721
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2022-12-13 23:15:41 -0600

    docstrings for cargoat.core.play

--- cargoat/core.py
+++ cargoat/core.py
@@ -8,6 +8,27 @@ from cargoat.errors import MontyHallError
 from cargoat.sim import MontyHallSim
 
 def play(game, n=100):
+    '''
+    Run a MontyHall simulation.
+
+    Parameters
+    ----------
+    game : list-like
+        A list of objects from the `cargoat.steps` subpackage.
+    n : int, optional
+        Number of games to simulate. The default is 100.
+
+    Raises
+    ------
+    MontyHallError
+        Problem with completing the game.
+
+    Returns
+    -------
+    sim : MontyHallSim
+        Simulation object, recording the trials and results.
+
+    '''
     sim = MontyHallSim(n=n)
     for i, step in enumerate(game):
         try:
