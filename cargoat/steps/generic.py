04ac5f65a8905135c3f9833af110cf1cb657c9b5
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2022-12-14 11:40:18 -0600

    add docstrings for GenericAction

--- cargoat/steps/generic.py
+++ cargoat/steps/generic.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This was a proposed implementation making steps for different actions
-more dependent on the same code.  It may be revisited in the future?
+Generic class to capture picking/unpicking, revealing/closing, and
+placing/removing cars.
 """
 
 from collections.abc import Iterable
@@ -13,41 +13,89 @@ from cargoat.arrayops import (n_per_row,
                               one_per_row,
                               one_per_row_weighted)
 
-# class _GenericActionSingleDoor():
-#     def __init__(self, target, pre_func=None, allowed_func=None,
-#                  behavior='overwrite', n_per_row=1,
-#                  allow_spoiled=False, allow_redundant=True):
-#         self.target = target
-#         self.pre_func = pre_func
-#         self.allowed_func = allowed_func
-#         self.behavior = behavior
-#         self.n_per_row = n_per_row
-#         self.allow_spoiled = allow_spoiled
-#         self.allow_redundant = allow_redundant
-
-#     def __call__(self, sim):
-
-#         if self.pre_func:
-#             self.pre_func(sim)
-
-#         if self.allowed_func is not None:
-#             allowed = self.allowed_func(sim)
-#         else:
-#             allowed = np.ones(sim.shape, dtype=bool)
-
-#         selections = one_per_row(sim.shape, allowed=allowed, dtype=int)
-#         sim._set_array(target=self.target,
-#                        new_array=selections,
-#                        behavior=self.behavior,
-#                        n_per_row=self.n_per_row,
-#                        allow_spoiled=self.allow_spoiled,
-#                        allow_redundant=self.allow_redundant)
-
 class GenericAction:
     def __init__(self, target, doors=1, weighted=False, behavior='overwrite',
                  exclude_picked=False, exclude_revealed=False, exclude_cars=False,
                  exclude_unpicked=False,exclude_closed=False, exclude_carless=False,
                  allow_spoiled=False, allow_redundant=True):
+        '''
+        Generic class for covering many actions in the Monty Hall game.
+
+        Most boil down to altering the cars, picked, or revealed array
+        of the MontyHallSim.  This class abstracts the specific array being
+        targeted,  allowing the same selection operations to be applied.
+
+        This action covers selecting 1 or more doors with uniform probability,
+        selecting 1 or more doors by index, or selecting one door with
+        weighted probabilities for each door.
+
+        This class is intended to cover picking, revealing, and placing cars,
+        as well as the negation of those actions.  Subclasses of this class
+        tailor/mask some of the options from GenericAction where appropriate.
+        See `cargoat.steps.pick`, `cargoat.steps.reveal`, and
+        `cargoat.steps.car` for more information.
+
+        Parameters
+        ----------
+        target : 'cars', 'picked', or 'revealed'
+            Attribute array of `cargoat.sim.MontyHallSim` to target.
+        doors : int or list-like, optional
+            Argument for specifying how many/which doors to select. The default is 1.
+            Possible options are as follows:
+            - a single integer is interpreted as the number of doors to
+            select (randomly, with equal probability)
+            - a list of integers with `weighted=False` is interpeted as
+            specific doors to pick, e.g. `[0, 2]` selects the doors at
+            index 0 and index 2.
+            - a list of integers with `weighted=True` is interpreted as
+            probabilities/weights for selecting a single door.  The length
+            of the weights can be either a) the same as the number of doors
+            or b) less than the number of doors.  In the latter case, the
+            number of weights must align with the number of selectable
+            doors per row, based on the `exclude_...` arguments below.
+        weighted : bool, optional
+            Treat the first argument as weights (see docs above).
+            The default is False.
+        behavior : 'add', 'overwrite', or 'remove', optional
+            How to treat the the new selections when applying them to the
+            simulation. The default is 'overwrite'.
+            - overwrite: new selections should simply replace the current target array.
+            - add: any 1s in the new selections should be added to any 1s in
+            the current target array (essentially an "or" operation)
+            - remove: any 1s in the new selection should indicate places to remove 1s
+            in the current array.
+        exclude_picked : bool, optional
+            Disallow selection of picked doors. The default is False.
+        exclude_revealed : bool, optional
+            Disallow selection of revealed doors. The default is False.
+        exclude_cars : bool, optional
+            Disallow selection of doors with cars. The default is False.
+        exclude_unpicked : bool, optional
+            Disallow selection of unpicked doors. The default is False.
+        exclude_closed : bool, optional
+            Disallow selection of closed doors. The default is False.
+        exclude_carless : bool, optional
+            Disallow selection of doors with goats. The default is False.
+        allow_spoiled : bool, optional
+            Do not throw an error of the game is spoiled. The default is False.
+            Spoiled games are recorded in the `spoiled` attribute of the
+            simulation.
+        allow_redundant : bool, optional
+            Do not throw an error if doors that are already set/unset are
+            selected to be redundantly set/unset (respectively).
+            The default is True.
+
+        Raises
+        ------
+        ValueError
+            `target` or `behavior` not in acceptable options, or problem
+            interpreting `doors` as weights when `weights=True`.
+
+        Returns
+        -------
+        None.
+
+        '''
 
         if target not in ('picked', 'revealed', 'cars'):
             raise ValueError('Target must be "picked", "revealed", '
