b11a97c70ed24c3d05d816f081f8764d0b648ef9
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2022-12-14 23:40:11 -0600

    docs for initialization

--- cargoat/steps/initialization.py
+++ cargoat/steps/initialization.py
@@ -10,6 +10,20 @@ import numpy as np
 
 class InitDoorsEmpty:
     def __init__(self, doors):
+        '''
+        Initialize the simulation with a given number
+        of doors.  All doors will contain goats.
+
+        Parameters
+        ----------
+        doors : int
+            Number of doors in simulation.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.doors = doors
 
     def __call__(self, sim):
@@ -20,7 +34,24 @@ class InitDoorsEmpty:
         sim.spoiled = np.zeros(sim.n, dtype=int)
 
 class InitDoorsFixed:
-    def __init__(self, placement=[0, 0, 1]):
+    def __init__(self, placement=(1, 0, 0)):
+        '''
+        Initialize the simulation with a known, constant
+        arrangement of cars and goats.
+
+        Parameters
+        ----------
+        placement : list-like, optional
+            Placement of cars & goats within each trial.
+            Zeros signify goats while 1s signify cars.
+            The default is (1, 0, 0), creating a three-door
+            simulation, all with placement goat-goat-car.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.placement = np.array(placement).astype(int)
 
     def __call__(self, sim):
@@ -32,6 +63,23 @@ class InitDoorsFixed:
 
 class InitDoorsRandom:
     def __init__(self, cars=1, goats=2):
+        '''
+        Initialize the doors with a given number of cars
+        and goats - the placement itself is random.  This
+        is closest to the original Monty Hall game.
+
+        Parameters
+        ----------
+        cars : int, optional
+            Number of cars per trial. The default is 1.
+        goats : int, optional
+            Number of goats per trial. The default is 2.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.cars = cars
         self.goats = goats
 
