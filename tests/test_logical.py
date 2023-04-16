e5d0507341d83e430cdd46bb22ef6867238097e6
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2023-04-16 18:32:57 -0500

    finish logical tests

--- tests/test_logical.py
+++ tests/test_logical.py
@@ -35,3 +35,67 @@ class TestChanceTo:
         sim = self.make_sim()
         action(sim)
         assert np.all(0 < sim.picked.astype(bool).sum(axis=1).sum() < 1000)
+
+class TestIfElse:
+
+    def make_sim(self):
+        sim = cg.MontyHallSim(1000)
+        sim.init_doors(3)
+        return sim
+
+    def test_callable(self):
+        sim = self.make_sim()
+        sim.cars[:500, :] = 1
+
+        condition = lambda x: x.cars.sum(axis=1) > 0
+
+        a = cg.Pick()
+        b = cg.Pass()
+
+        action = cg.IfElse(condition, a, b, call=True)
+        action(sim)
+
+        assert np.all(sim.count_totals('picked') == np.repeat([1, 0], [500, 500]))
+
+    def test_not_callable(self):
+        sim = self.make_sim()
+        sim.cars[:500, :] = 1
+
+        condition = np.repeat([True, False], [500, 500])
+
+        a = cg.Pick()
+        b = cg.Pass()
+
+        action = cg.IfElse(condition, a, b, call=False)
+        action(sim)
+
+        assert np.all(sim.count_totals('picked') == np.repeat([1, 0], [500, 500]))
+
+class TryExcept:
+
+    def make_sim(self):
+        sim = cg.MontyHallSim(1000)
+        sim.init_doors(3)
+        return sim
+
+    def test_fail(self):
+
+        a = lambda x : 1/0
+        b = cg.Pick()
+
+        sim = self.make_sim()
+        action = cg.TryExcept(a, b)
+        action(sim)
+
+        assert np.all(sim.count_totals('picked') == 1)
+
+    def test_no_fail(self):
+
+        a = cg.Reveal()
+        b = cg.Pick()
+
+        sim = self.make_sim()
+        action = cg.TryExcept(a, b)
+        action(sim)
+
+        assert np.all(sim.count_totals('picked') == 0) and np.all(sim.count_totals('revealed') == 1)
